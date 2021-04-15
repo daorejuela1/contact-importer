8146908bb42eaa7c8d9ff9a7a07865c4b0861c03
daorejuela1 <daorejuela1@outlook.com>
2021-04-15 05:42:14 +0000

    Added test to work with S3 files

--- app/jobs/contact_worker_job.rb
+++ app/jobs/contact_worker_job.rb
@@ -15,7 +15,11 @@ class ContactWorkerJob
     contact_error_saved = false
 
     csv_file_name = csv_file.csv_file.filename.to_s
-    file_path = ActiveStorage::Blob.service.path_for(csv_file.csv_file.key)
+    #file_path = ActiveStorage::Blob.service.path_for(csv_file.csv_file.key)
+    #file_path = ActiveStorage::Downloader.download_blob_to_tempfile(csv_file.csv_file.key)
+    
+    #file = ActiveStorage::Downloader.new(csv_file.csv_file).download_blob_to_tempfile.path
+    file_path = csv_file.csv_file.service_url
     headers = nil
     CSV.open(File.open(file_path), "r+") do |file|
       headers = file.shift
