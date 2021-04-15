9a5e72ed624d89b6c0a0eff09525afb719baeeed
daorejuela1 <daorejuela1@outlook.com>
2021-04-15 03:21:49 +0000

    Set up sidekiq to connect with redis by using ENV variables

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,1 @@
 require 'sidekiq'
-Sidekiq.configure_server do |config|
-  config.redis = {url: 'redis://localhost:6379/0'}
-end
-Sidekiq.configure_client do |config|
-  config.redis = {url: 'redis://localhost:6379/0'}
-end
